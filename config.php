47dc22bf0175c228d411aa24caa2f5370f5afabf
SuperUser <azimbekxaydarov@gmail.com>
2023-04-20 17:54:41 +0500

    make config file

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'ware_houses');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
--- database/data_inserter.php
+++ database/data_inserter.php
@@ -6,13 +6,15 @@ class DataInserter extends DB_Connection
 
     public function create($table, $values, $columns = null)
     {
-        $columns = implode(', ', $columns);
-        $values = "'" . implode("', '", $values) . "'";
-        $sql = "INSERT INTO $table ({$columns}) VALUES ({$values})";
-        $this->connect()->query($sql);
-        if ($this->connect()->connect_error) {
-            die("Error executing file:  " . $this->connect()->connect_error);
+        if ($table && $values) {
+            $columns = implode(', ', $columns);
+            $values = "'" . implode("', '", $values) . "'";
+            $sql = "INSERT INTO $table ({$columns}) VALUES ({$values})";
+            $this->connect()->query($sql);
+            if ($this->connect()->connect_error) {
+                die("Error executing file:  " . $this->connect()->connect_error);
+            }
+            return true;
         }
-        return true;
     }
 }
