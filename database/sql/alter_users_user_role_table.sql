6f066547dae704bea31ac136e679eae9717a74c0
SuperUser <azimbekxaydarov@gmail.com>
2023-04-21 18:33:05 +0500

    small mistake fixed

--- database/sql/alter_users_user_role_table.sql
+++ database/sql/alter_users_user_role_table.sql
@@ -1,1 +1,1 @@
-ALTER TABLE `user_role` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`), ADD UNIQUE KEY `unique_user_role` (`user_id`, `role_id`);
+ALTER IGNORE TABLE `user_role` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`), ADD UNIQUE KEY `unique_user_role` (`user_id`, `role_id`);
