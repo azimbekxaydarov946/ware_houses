3e98cd22e876a4ef5ed20670583a0e9309c8ccd0
SuperUser <azimbekxaydarov@gmail.com>
2023-04-23 18:25:38 +0500

    Assign user roles

--- form/user-create.php
+++ form/user-create.php
@@ -1,5 +1,17 @@
 <?php
 include "layout-top-form.php";
+require "../database/database_connection.php";
+
+$db = new DB_Connection('localhost', 'root', '', 'ware_houses');
+
+if (isset($db)) {
+    $query = $db->connect()->query("SELECT name FROM `roles`");
+    if ($query->num_rows > 0) {
+        while ($row = $query->fetch_object()) {
+            $request[] = $row;
+        }
+    }
+}
 ?>
 
 
@@ -15,6 +27,21 @@ include "layout-top-form.php";
     <div class="form-group">
         <label class="form-control-label">Password</label>
         <input type="password" placeholder="Password" class="form-control" name="password">
+    </div>
+
+    <div class="form-group">
+        <label class="form-control-label">Role</label>
+        <select name="role" id="" class="form-control">
+            <?php
+            if (isset($request)) :
+                foreach ($request as $key => $item) :
+            ?>
+                    <option value="<?= $item->name ?>"><?= $item->name ?></option>
+            <?php
+                endforeach;
+            endif;
+            ?>
+        </select>
     </div>
     <div class="form-group">
         <input type="submit" value="save" class="btn btn-primary">
